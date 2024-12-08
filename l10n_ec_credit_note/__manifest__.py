b6c963c0433331c8288bc9da1ca574ef25dff90d
Jorge Luis <jorgequiguango@outlook.com>
2024-12-07 23:03:00 -0500

    [ADD] Modules l10n_ec

--- l10n_ec_credit_note/__manifest__.py
+++ l10n_ec_credit_note/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Odoo Community Association (OCA), Odoo-EC",
     "website": "https://github.com/OCA/l10n-ecuador",
     "license": "AGPL-3",
-    "version": "18.0.1.0.0",
+    "version": "17.0.1.0.0",
     "depends": [
         "account",
         "account_edi",
