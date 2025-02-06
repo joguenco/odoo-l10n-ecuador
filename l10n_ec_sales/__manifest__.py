ac466028c0c6a081a35538089cf5bf9e029b3b20
Jorge Luis <jorgequiguango@outlook.com>
2025-02-06 18:13:03 -0500

    [FIX] Pre commit

--- l10n_ec_sales/__manifest__.py
+++ l10n_ec_sales/__manifest__.py
@@ -1,17 +1,16 @@
-# coding: utf-8
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Report - Sale order ticket',
-    'version': '1.0',
-    'description': 'Sale modifications for ticket order',
-    'category': 'Localization',
-    'depends': [
-        'sale',
+    "name": "Report - Sale order ticket",
+    "version": "18.0.1.0.0",
+    "category": "Localization",
+    "author": "Jorge Luis, Odoo Community Association (OCA)",
+    "depends": [
+        "sale",
     ],
-    'data': [
-        'report/sale_order_templates.xml',
+    "data": [
+        "report/sale_order_templates.xml",
     ],
-    'installable': True,
-    'auto_install': True,
-    'license': 'LGPL-3',
+    "installable": True,
+    "auto_install": True,
+    "license": "LGPL-3",
 }
