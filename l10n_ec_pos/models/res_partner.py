2ef37b23a65f94e6cbce7aa455d4e395722de3cb
Jorge Luis <jorgeluis@resolvedor.dev>
2025-05-24 22:52:57 -0500

    [IMP] Validate CI

--- l10n_ec_pos/models/res_partner.py
+++ l10n_ec_pos/models/res_partner.py
@@ -1,15 +1,63 @@
-from odoo import models, fields, api
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
+    _inherit = "res.partner"
 
     @api.model
     def _get_default_country(self):
-
-        country = self.env['res.country'].search([('code', '=', 'EC')], limit=1)
+        country = self.env["res.country"].search([("code", "=", "EC")], limit=1)
 
         return country
-    
-    country_id = fields.Many2one('res.country', string='Country', default=_get_default_country)
+
+    country_id = fields.Many2one(
+        "res.country", string="Country", default=_get_default_country
+    )
+
+    @api.constrains("vat", "country_id", "l10n_latam_identification_type_id")
+    def check_vat(self):
+        result = super().check_vat()
+        (valid, message) = self.l10n_ec_validate_ci(self.vat)
+        if not valid:
+            raise ValidationError(_(message))
+
+        return result
+
+    @api.onchange("vat")
+    def _onchange_vat(self):
+        if self.vat:
+            super().check_vat()
+            (valid, message) = self.l10n_ec_validate_ci(self.vat)
+            if not valid:
+                raise ValidationError(_(message))
+
+    def l10n_ec_validate_ci(self, identification) -> tuple[bool, str]:
+        province = int(identification[0:2])  # dos primeros dígitos de la CI
+        if 1 <= province <= 24 or province == 30:
+            third_digit = int(identification[2])
+            # El tercer dígito debe estar entre 0 y 6
+            if 0 <= third_digit <= 6:
+                validator = int(identification[9])
+                coefficients = (2, 1, 2, 1, 2, 1, 2, 1, 2)  # coeficientes del módulo 10
+                accumulated = 0
+                for i in range(0, len(coefficients)):
+                    multiplication = int(identification[i]) * coefficients[i]
+                    # Si una multiplicación es >= 10 se le debe restar 9
+                    if multiplication >= 10:
+                        multiplication -= 9
+                    # print(f'Digito {int(cedula[i])}, multiplicacion {multip}')
+                    accumulated += multiplication
+                module = accumulated % 10  # calculamos el módulo 10
+                subtract = (
+                    10 - module
+                )  # para calcular el validador restamos de 10 el módulo obtenido
+
+                if subtract == validator or (module == 0 and validator == 0):
+                    return True, "La cédula es válida"
+                else:
+                    return False, "La cédula no es válida"
+            else:
+                return False, "El tercer dígito no es válido"
+        else:
+            return False, "El código de provincia no es válido"
